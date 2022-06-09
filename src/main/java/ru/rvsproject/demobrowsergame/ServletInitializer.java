d1267f785f9c5a58b86cbcdb26b831abc7649e3f
Vadim Rodionov <tuukvadim@live.com>
2022-06-09 18:35:37 +0300

    Инициализация! Начнём работу по созданию игры

---
+++ src/main/java/ru/rvsproject/demobrowsergame/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.rvsproject.demobrowsergame;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoGameApplication.class);
+    }
+
+}
---
+++ src/test/java/ru/rvsproject/demobrowsergame/DemoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rvsproject.demobrowsergame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
